b356e201636a12b1d2b9e98272ea7be4713e8282
veeresh <veeresh@monnai.com>
2023-09-17 11:04:03 +0530

    fixed build issues

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,23 +1,23 @@
 import React from "react";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
-let POSTS = [
-  {
-    label: "post1",
-    key: "1",
-  },
-  {
-    label: "post2",
-    key: "2",
-  },
-  {
-    label: "post3",
-    key: "3",
-  },
-];
+// let POSTS = [
+//   {
+//     label: "post1",
+//     key: "1",
+//   },
+//   {
+//     label: "post2",
+//     key: "2",
+//   },
+//   {
+//     label: "post3",
+//     key: "3",
+//   },
+// ];
 
 const Home = () => {
-  const queryClient = useQueryClient();
+  // const queryClient = useQueryClient();
   const postQuery = useQuery({
     queryKey: ["posts"],
     queryFn: () =>
@@ -26,15 +26,15 @@ const Home = () => {
         .then((json) => json),
   });
 
-  const mutationQuery = useMutation({
-    mutationFn: (title: string) =>
-      wait(1000).then(() => {
-        POSTS.push({ key: Math.random().toString(), label: title });
-      }),
-    onSuccess: () => {
-      queryClient.invalidateQueries(["posts"]);
-    },
-  });
+  // const mutationQuery = useMutation({
+  //   mutationFn: (title: string) =>
+  //     wait(1000).then(() => {
+  //       POSTS.push({ key: Math.random().toString(), label: title });
+  //     }),
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries(["posts"]);
+  //   },
+  // });
 
   console.log(postQuery.data, "postQuery");
 
@@ -54,6 +54,6 @@ const Home = () => {
 
 export default Home;
 
-const wait = (duration: any) => {
-  return new Promise((resolve) => setTimeout(resolve, duration));
-};
+// const wait = (duration: any) => {
+//   return new Promise((resolve) => setTimeout(resolve, duration));
+// };
